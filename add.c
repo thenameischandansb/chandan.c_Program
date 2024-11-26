bd5a765030c866feecc4b61c3a5db0962858b6a2
Chandan.S.B <chandansbchandansb6@gmail.com>
2024-11-26 12:41:02 +0530

    Update add.c

--- add.c
+++ add.c
@@ -2,19 +2,11 @@
 
 int main() {
     float num1, num2, sum;
-
-    // Taking input from the user
     printf("Enter the first number: ");
     scanf("%f", &num1);
-
     printf("Enter the second number: ");
     scanf("%f", &num2);
-
-    // Adding the two numbers
     sum = num1 + num2;
-
-    // Displaying the result
     printf("The sum of %.2f and %.2f is %.2f\n", num1, num2, sum);
-
     return 0;
 }
