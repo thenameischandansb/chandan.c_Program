d5a44b43e605b5fdb6b3f6d78f6dca0f3cde4348
Chandan.S.B <chandansbchandansb6@gmail.com>
2024-11-27 11:28:05 +0530

    Add files via upload

---
+++ call by value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;  
+    *a = *b;        
+    *b = temp;     
+}
+int main() {
+    int a = 5, b = 10; 
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    swap(&a, &b); 
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
